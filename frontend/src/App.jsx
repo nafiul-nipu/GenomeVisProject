5570141304c9076d7c094da8ffd30f59279e5f64
Md Nafiul Alam Nipu <mnipu2@uic.edu>
2025-02-07 19:42:26 -0600

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+const { Header, Content, Footer } = Layout;
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <Layout>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          color: "black",
+          backgroundColor: "white",
+          height: "5vh",
+        }}
+      >
+        Navigation Panel
+      </Header>
+      <Content>
+        <div
+          style={{
+            width: "100vw",
+            height: "90vh",
+          }}
+        >
+          content
+        </div>
+      </Content>
+    </Layout>
+  );
+};
+export default App;
