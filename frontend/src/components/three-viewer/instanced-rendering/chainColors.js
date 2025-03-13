160ffc107161a237166c390f8cbd90c02351ddc5
Md Nafiul Alam Nipu <mnipu2@uic.edu>
2025-03-13 01:57:34 -0500

    normalizeposition.. TODO update geneWithAcc

--- frontend/src/components/three-viewer/instanced-rendering/chainColors.js
+++ frontend/src/components/three-viewer/instanced-rendering/chainColors.js
@@ -1,19 +1,32 @@
 // Desc: Chain colors for the instanced rendering of the network
 export const chain_colors = {
-  k: "rgb(41, 128, 185)", // Navy Blue
-  m: "rgb(52, 73, 94)", // Dark Gray
-  l: "rgb(236, 112, 99)", // Salmon
-  p: "rgb(82, 190, 128)", // Mint Green
-  q: "rgb(31, 97, 141)", // Dark Blue
-  r: "rgb(211, 84, 0)", // Pumpkin Orange
-  s: "rgb(108, 52, 131)", // Dark Purple
-  chr01: "rgb(255, 195, 0)", // Yellow
-  chr05: "rgb(133, 193, 233)", // Blue
-  chr10: "rgb(39, 174, 96)", // Olive Green
-  chr13: "rgb(243, 156, 18)", // Orange
-  chr17: "rgb(52, 152, 219)", // Light Blue
-  chr19: "rgb(187, 143, 206)", // Lilac
-  chr21: "rgb(26, 188, 156)", // Turquoise
-  chr26: "rgb(76, 175, 80)", // Green
-  chr27: "rgb(155, 89, 182)", // Purple
+  chr1: "rgb(255, 195, 0)", // Yellow
+  chr2: "rgb(133, 193, 233)", // Blue
+  chr3: "rgb(39, 174, 96)", // Olive Green
+  chr4: "rgb(243, 156, 18)", // Orange
+  chr5: "rgb(52, 152, 219)", // Light Blue
+  chr6: "rgb(187, 143, 206)", // Lilac
+  chr7: "rgb(26, 188, 156)", // Turquoise
+  chr8: "rgb(76, 175, 80)", // Green
+  chr9: "rgb(155, 89, 182)", // Purple
+  chr10: "rgb(41, 128, 185)", // Navy Blue
+  chr11: "rgb(52, 73, 94)", // Dark Gray
+  chr12: "rgb(236, 112, 99)", // Salmon
+  chr13: "rgb(82, 190, 128)", // Mint Green
+  chr14: "rgb(31, 97, 141)", // Dark Blue
+  chr15: "rgb(211, 84, 0)", // Pumpkin Orange
+  chr16: "rgb(108, 52, 131)", // Dark Purple
+  chr17: "rgb(255, 195, 0)", // Yellow
+  chr18: "rgb(133, 193, 233)", // Blue
+  chr19: "rgb(39, 174, 96)", // Olive Green
+  chr20: "rgb(243, 156, 18)", // Orange
+  chr21: "rgb(52, 152, 219)", // Light Blue
+  chr22: "rgb(187, 143, 206)", // Lilac
+  chr23: "rgb(26, 188, 156)", // Turquoise
+  chr24: "rgb(76, 175, 80)", // Green
+  chr25: "rgb(155, 89, 182)", // Purple
+  chr26: "rgb(41, 128, 185)", // Navy Blue
+  chr27: "rgb(52, 73, 94)", // Dark Gray
+  chr28: "rgb(236, 112, 99)", // Salmon
+  chr29: "rgb(82, 190, 128)", // Mint Green
 };
--- frontend/src/utilities/normalizePoistionCalculation.js
+++ frontend/src/utilities/normalizePoistionCalculation.js
@@ -1,28 +1,33 @@
-export default function normalizePoistionCalculation(data, scaleFactor = 15) {
+export default function normalizePoistionCalculation(
+  data,
+  pos_attr_name,
+  scaleFactor = 15
+) {
   //   console.log(data);
 
-  const middlePositions = data.map((pos) => pos.middle_pos);
+  const positions = data.map((pos) => pos[pos_attr_name]);
 
-  const minX = Math.min(...middlePositions.map((pos) => pos[0]));
-  const maxX = Math.max(...middlePositions.map((pos) => pos[0]));
-  const minY = Math.min(...middlePositions.map((pos) => pos[1]));
-  const maxY = Math.max(...middlePositions.map((pos) => pos[1]));
-  const minZ = Math.min(...middlePositions.map((pos) => pos[2]));
-  const maxZ = Math.max(...middlePositions.map((pos) => pos[2]));
+  const minX = Math.min(...positions.map((pos) => pos[0]));
+  const maxX = Math.max(...positions.map((pos) => pos[0]));
+  const minY = Math.min(...positions.map((pos) => pos[1]));
+  const maxY = Math.max(...positions.map((pos) => pos[1]));
+  const minZ = Math.min(...positions.map((pos) => pos[2]));
+  const maxZ = Math.max(...positions.map((pos) => pos[2]));
 
   const normalize = (value, min, max) => (value - min) / (max - min);
 
   const updatedPositions = data.map((pos) => {
     const normalizedPos = [
-      normalize(pos.middle_pos[0], minX, maxX) * scaleFactor, // Normalize and scale X
-      normalize(pos.middle_pos[1], minY, maxY) * scaleFactor, // Normalize and scale Y
-      normalize(pos.middle_pos[2], minZ, maxZ) * scaleFactor, // Normalize and scale Z
+      normalize(pos[pos_attr_name][0], minX, maxX) * scaleFactor, // Normalize and scale X
+      normalize(pos[pos_attr_name][1], minY, maxY) * scaleFactor, // Normalize and scale Y
+      normalize(pos[pos_attr_name][2], minZ, maxZ) * scaleFactor, // Normalize and scale Z
     ];
 
     // Return a new object with the updated middle_pos
     return {
       ...pos, // Preserve all other properties
-      middle_pos: normalizedPos, // Update middle_pos
+      [pos_attr_name]: normalizedPos,
+      original_pos: pos[pos_attr_name],
     };
   });
 
